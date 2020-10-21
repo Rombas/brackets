05f59119cc65fe082f029bce780c29308828cac0
Rombas <RomanBash@gmail.com>
2020-10-21 14:59:39 +0300

    feat: realized brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let count = 0;
+  for (let i = 0; i < bracketsConfig.length; i++){
+    for (let j = 0; j < str.length; j++){
+      if (str[j] === bracketsConfig[i][0])
+      {
+          for (let n = j + 1; n < str.length; n++){
+            if (str[n] === bracketsConfig[i][1] && count === 0){
+              if ((n - j) % 2 == 0) return false;
+              str = str.slice(0,j) + str.slice(j + 1 , str.length);
+              str = str.slice(0,n-1) + str.slice(n , str.length);
+              n = str.length;
+              j--;
+              if (str === '') return true;
+            } else if (str[n] === bracketsConfig[i][0]) 
+            {
+              count++;
+            } else if (str[n] === bracketsConfig[i][1]) 
+            {
+            count--;
+            } 
+          }
+      }
+    }
+  }
+  return false;
 }
